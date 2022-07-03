14b65cf75171bcd9f02f235e07112d5e4a1d2b67
Bernardo Barardo <bernardo.s.barardo@gmail.com>
2022-07-03 18:26:17 +0100

    Setup tailwind and daisy-ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <button className='btn'>Hello daisyUI</button>
+    </div>
+  );
+};
+
+export default Home;
